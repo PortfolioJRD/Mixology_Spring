77dfb6d6788bb2e59fd39d5a7e19cf6bb41f2746
Jinyeong Kim <jkim495@myseneca.ca>
2021-04-15 16:29:37 -0400

    initial commit

---
+++ src/test/java/com/mixology/Mixology2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mixology2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
